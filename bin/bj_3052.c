6aef54d2791016573817112a73ba1ad7e08147cb
sungh313 <gocamp12313@gmail.com>
2021-07-03 07:58:55 +0000

    3052문제 해결

--- bin/bj_3052.c
+++ bin/bj_3052.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
+
 int main(){
-	int ar1[10];
-	int ar2[10] = {0, };
+	int ar1[11];
+	int ar2[13] = {0, };
 	int i, l, x, y;
 	
 	y = 0;
+	l = 0;
 	
 	for(i=0;i<10;i++){
 		scanf("%d", &ar1[i]);
@@ -13,14 +15,34 @@ int main(){
 	for(i=0;i<10;i++){
 		x = ar1[i]%42;
 		
-		for(l=0;l<i;l++){
-			if(ar2[l]!=x){
-				ar2[i] = x;
-				y = y+1;
-				break;
-			}
+		//printf("%d\n", x);
+		
+		if(y==0){
+			ar2[0] = x;
+			y = 1;
+			//printf("실행검증\n");
+		}
+		
+		l = 0;
+		while(l<y){
+				if(ar2[l]!=x && l==(y-1)){
+					ar2[y] = x;
+					y = y+1;
+					//printf("x=%d, y=%d, 배열값%d\n", x, y, ar2[y-1]);
+					break;
+				}
+				else if(ar2[l]==x) {
+					//printf("%d, eles문 실행\n", x);
+					break;
+				}
+				l++;
+				//printf("%d \n", l);
+				//printf("x = %d, l = %d, y = %d\n", x, l, y);
 		}
 	}
 	
 	printf("%d\n", y);
+	//for(i=0;i<y;i++){
+	//	printf("%d\n", ar2[i]);
+	//}
 }
